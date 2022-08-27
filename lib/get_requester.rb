8422cd37131d1855d18cdd1a2888ebb025f546eb
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-08-27 06:18:17 +0300

    commit tree 2e61d8205aad62ddd83f5b68490c052e5636c8f4

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,8 @@
 # Write your code here
+require 'net/http'
+require 'json'
+require 'open-uri'
+
+class GetRequester
+    URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+end
